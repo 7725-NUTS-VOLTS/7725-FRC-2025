936dd5fafcb74d225264b4682bb662c88a279b05
Camila-Rabadan <camila.rabadan09@gmail.com>
2025-10-11 17:12:28 -0600

    ELEVADOR YEI

---
+++ src/main/java/frc/robot/subsystems/ElevatorSubsystem.java
@@ -0,0 +1,160 @@
+package frc.robot.subsystems;
+import static edu.wpi.first.units.Units.Centimeter;
+import static edu.wpi.first.units.Units.Centimeters;
+import static edu.wpi.first.units.Units.Meters;
+import static edu.wpi.first.units.Units.MetersPerSecond;
+import static edu.wpi.first.units.Units.Volts;
+import com.revrobotics.RelativeEncoder;
+import com.revrobotics.spark.ClosedLoopSlot;
+import com.revrobotics.spark.SparkBase.ControlType;
+import com.revrobotics.spark.SparkBase.PersistMode;
+import com.revrobotics.spark.SparkBase.ResetMode;
+import com.revrobotics.spark.SparkClosedLoopController;
+import com.revrobotics.spark.SparkMax;
+import com.revrobotics.spark.SparkLowLevel.MotorType;
+import com.revrobotics.spark.config.ClosedLoopConfig.FeedbackSensor;
+import com.revrobotics.spark.config.SparkBaseConfig.IdleMode;
+import com.revrobotics.spark.config.SparkMaxConfig;
+
+import edu.wpi.first.units.measure.MutDistance;
+import edu.wpi.first.units.measure.MutLinearVelocity;
+import edu.wpi.first.units.measure.MutVoltage;
+import edu.wpi.first.units.measure.Voltage;
+import edu.wpi.first.units.measure.MutDistance;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import edu.wpi.first.wpilibj2.command.sysid.SysIdRoutine;
+
+
+public class ElevatorSubsystem extends SubsystemBase {
+    private static ElevatorSubsystem instance = null;
+
+    private SparkMax masterMotor, followerMotor;
+    private SparkMaxConfig masterMotorConfig, followerMotorConfig;
+    private SparkClosedLoopController closedLoopController;
+    private RelativeEncoder encoder;
+
+    private static double defaultPose = 0;
+
+    private ElevatorSubsystem(){
+        masterMotor = new SparkMax(0, null); // ADD MOTORID 28
+        followerMotor = new SparkMax(0, null); // ADD MOTORID
+
+        closedLoopController = masterMotor.getClosedLoopController();
+
+        masterMotorConfig = new SparkMaxConfig();
+        followerMotorConfig = new SparkMaxConfig();
+
+        masterMotorConfig.idleMode(IdleMode.kCoast);
+        followerMotorConfig.idleMode(IdleMode.kCoast);
+
+        masterMotorConfig.smartCurrentLimit(0); //ADD CONSTANTS
+        followerMotorConfig.smartCurrentLimit(0); //ADD CONSTANTS
+
+        masterMotorConfig.encoder
+            .positionConversionFactor(
+                2* Math.PI * 0/0 )
+            .velocityConversionFactor(
+                2*Math.PI* 0/0);
+
+        masterMotorConfig.closedLoop
+            .feedbackSensor(FeedbackSensor.kPrimaryEncoder)
+            .p(0) //ADD CONSTANTS
+            .i(0) // ADD CONSTANTS
+            .d(0); //ADD CONSTANTS
+
+        masterMotorConfig.closedLoop.maxMotion
+            .maxVelocity(0) //ADD CONSTANTS
+            .maxAcceleration(0) //ADD CONSTANTS
+            .allowedClosedLoopError(0); //ADD CONSTANTS
+
+        masterMotorConfig.inverted(false); //ADD CONSTANTS
+        followerMotorConfig.inverted(!false); //ADD CONSTANTS
+
+        masterMotor.configure(masterMotorConfig, ResetMode.kResetSafeParameters, PersistMode.kPersistParameters);
+        followerMotorConfig.follow(masterMotor, true);
+        followerMotor.configure(followerMotorConfig,  ResetMode.kResetSafeParameters, PersistMode.kPersistParameters);
+
+        encoder = masterMotor.getEncoder();
+
+        routine = new SysIdRoutine(
+           new SysIdRoutine.Config(null, Voltage.ofBaseUnits(4, Volts), null),
+           new SysIdRoutine.Mechanism(
+            masterMotor::setVoltage,
+            log -> {
+
+                log.motor("elevator")
+                .voltage(
+                    m_appliedVoltage.mut_replace(
+                        masterMotor.getAppliedOutput(),Volts))
+                        .linearPosition(m_angle.mut_replace(encoder.getPosition(), Meters))
+                        .linearVelocity(m_velocity.mut_replace(encoder.getVelocity(), MetersPerSecond));
+
+            }, this
+           )
+        );
+    }
+
+    public void resetPosition(){
+        encoder.setPosition(0);
+        defaultPose = 0;
+    }
+
+    public void moveElevatotToPose(double point){
+        closedLoopController.setReference(point, ControlType.kMAXMotionPositionControl, ClosedLoopSlot.kSlot0);
+
+    }
+
+    public void stop(){
+        masterMotor.stopMotor();
+    }
+
+    public void setPower(double power){
+        masterMotor.setVoltage(power*12);
+    }
+
+    public boolean isInPoint(double point){
+        return(Math.abs(encoder.getPosition()-point) <= 0); //ADD CONSTANTS
+    }
+
+    public double getPose(){
+        return encoder.getPosition();
+    }
+
+    public void setDefaultPose(double pose){
+        defaultPose = pose;
+    }
+
+    public static double getDefaultPose(){
+        return defaultPose;
+    }
+
+    public static ElevatorSubsystem getInstance(){
+        if(instance==null){
+            instance = new ElevatorSubsystem();
+        }
+        return instance;
+    }
+
+    @Override
+    public void periodic(){
+        SmartDashboard.putNumber("elevatorPose", encoder.getPosition());
+        SmartDashboard.putNumber("elevatorSpeed", encoder.getVelocity());
+        SmartDashboard.putNumber("elevatorCurrent", masterMotor.getOutputCurrent());
+    }
+
+    public Command sysIdQuasistatic(SysIdRoutine.Direction direction){
+        return routine.quasistatic(direction);
+    }
+
+    public Command sysIdDynamic(SysIdRoutine.Direction direction){
+        return routine.dynamic(direction);
+    }
+
+    private final MutVoltage m_appliedVoltage = Volts.mutable(0);
+    private final MutDistance m_angle= Centimeters.mutable(0);
+    private final MutLinearVelocity m_velocity = MetersPerSecond.mutable(0);
+
+    private final SysIdRoutine routine;
+}
