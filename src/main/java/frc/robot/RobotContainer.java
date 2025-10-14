b08748b216602b33b1958420b06c0fb7a2f46b2b
NutsAndVolts7725 <nutsandvrobotics@gmail.com>
2025-10-13 19:32:05 -0600

    Elevator Tunned Constants

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -11,7 +11,7 @@ import edu.wpi.first.wpilibj2.command.InstantCommand;
 import edu.wpi.first.wpilibj2.command.ParallelCommandGroup;
 import edu.wpi.first.wpilibj2.command.button.JoystickButton;
 import frc.robot.Constants.BrazoConstants;
-import frc.robot.Constants.PelotaConstants;
+import frc.robot.Constants.ElevaotrConstants;
 import frc.robot.Constants.EscupidorConstants;
 import frc.robot.commands.*;
 import frc.robot.subsystems.*;
@@ -50,9 +50,13 @@ public class RobotContainer {
     
     // example: private final JoystickButton nombreAccion = new JoystickButton(opperator, XboxController.Button.k{nombreButton}.value)
     
+    private final JoystickButton ejectCoral = new JoystickButton(opperator, XboxController.Button.kA.value);
+    private final JoystickButton retractCoral = new JoystickButton(opperator, XboxController.Button.kY.value);
+
+    
     /* Subsystems */
     private final Swerve s_Swerve = new Swerve();
-   
+   private final EscupidorSubSystem escupidorSubSystem = new EscupidorSubSystem();
 
     /*Auto Selector on Dashboard*/
     private SendableChooser<Command> autoChooser;
@@ -108,9 +112,11 @@ public class RobotContainer {
         /* Driver Buttons */
         zeroGyro1.and(zeroGyro2).onTrue(new InstantCommand(() -> s_Swerve.zeroHeading()));
         resetWheels.onTrue(new InstantCommand(() -> s_Swerve.resetModulesToAbsolute()));
+        /* Operator Buttons */
+
     }
 
-        /* Operator Buttons */
+        
      
 
     /*
--- src/main/java/frc/robot/commands/ElevatorDefaultCommand.java
+++ src/main/java/frc/robot/commands/ElevatorDefaultCommand.java
@@ -5,23 +5,24 @@ import edu.wpi.first.math.controller.ProfiledPIDController;
 import edu.wpi.first.math.trajectory.TrapezoidProfile;
 import edu.wpi.first.wpilibj2.command.Command;
 import frc.robot.subsystems.ElevatorSubsystem;
+import frc.robot.Constants.ElevaotrConstants;
 
 public class ElevatorDefaultCommand extends Command {
     private ElevatorSubsystem elevator;
     private double power;
 
-    private final TrapezoidProfile.Constraints m_Constraints = new TrapezoidProfile.Constraints(0, 0);
+    private final TrapezoidProfile.Constraints m_Constraints = new TrapezoidProfile.Constraints(ElevaotrConstants.ELEVATOR_MAX_VELO, ElevaotrConstants.ELEVATOR_MAX_ACCELLERATION);
 
-    private final ProfiledPIDController pidController = new ProfiledPIDController(0, 0, 0, m_Constraints);
+    private final ProfiledPIDController pidController = new ProfiledPIDController(ElevaotrConstants.ELEVATOR_P, ElevaotrConstants.ELEVATOR_I, ElevaotrConstants.ELEVATOR_D, m_Constraints);
 
-    ElevatorFeedforward elevatorFeedforward= new ElevatorFeedforward(0, 0, 0);
+    ElevatorFeedforward elevatorFeedforward= new ElevatorFeedforward(0.0086531, 0.5, 0.000215);
 
     public ElevatorDefaultCommand() {
         elevator = ElevatorSubsystem.getInstance();
         this.power = 0;
         addRequirements();
 
-        pidController.setTolerance(0);
+        pidController.setTolerance(ElevaotrConstants.ELEVATOR_POSITION_TOLERANCE);
     }
 
     @Override
